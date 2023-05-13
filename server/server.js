92d69482f2f2ddf027cdb4f47019d3a5bd8902e8
loique70 <fogouangloic10@gmail.com>
2023-05-13 12:44:17 -0700

    updated folder

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const app = express()
-const connectDB = require('./config/connectDB.js')
+const connectDB = require('./src/config/connectDB.js')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const PORT = process.env.PORT
@@ -12,10 +12,10 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/', express.static(path.join(__dirname, '/public')))
-app.use('/', require('./routes/root'))
-app.use('/categories', require('./routes/categoriesRoute'))
-app.use('/transaction', require('./routes/transactionRoute'))
-app.use('/label', require('./routes/labelRoute'))
+app.use('/', require('./src/routes/root.js'))
+app.use('/categories', require('./src/routes/categoriesRoute'))
+app.use('/transaction', require('./src/routes/transactionRoute'))
+app.use('/label', require('./src/routes/labelRoute'))
 
 //Cette route va permettre de tous les pages et routes inexitente que l'on va essayer de joindre.
 app.all('*', (req,res) => {
